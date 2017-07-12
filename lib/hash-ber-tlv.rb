49b9634b609ce2be2715b986c57bd7cb75063f23
Thiago Scalone <thiago@scalone.com.br>
2017-07-12 16:51:02 -0300

    Refactoring Hash.from_ber_tlv replacing while for loop do, and add a test case.

--- lib/hash-ber-tlv.rb
+++ lib/hash-ber-tlv.rb
@@ -1,68 +1,68 @@
 # encoding: utf-8
 class Hash
-	def from_ber_tlv(data)
-		i = 0
-		tag = data[i]
-		if tag.unpack('C').pop & 0x1F == 0x1F # matches 00011111 mask? 
-			begin
-				i += 1
-				tag[i] = data[i]
-			end while tag[i].unpack('C').pop & 0x80 == 0x80
-		end
+  def from_ber_tlv(data)
+    i = 0
+    tag = data[i]
+    if (tag.unpack('C').pop & 0x1F) == 0x1F # matches 00011111 mask? 
+      loop do
+        i += 1
+        tag[i] = data[i]
+        break unless (tag[i].unpack('C').pop & 0x80) == 0x80
+      end
+    end
 
-		length = data[tag.length].unpack('C').pop
+    length = data[tag.length].unpack('C').pop
 
-		tag_i = tag.unpack('H*').pop.to_i(16)
+    tag_i = tag.unpack('H*').pop.to_i(16)
 
-		if length < 0x80
-			self[tag_i] = data[tag.length + 1, length]
-			remainder = data[tag.length + 1 + length..-1]
-		else
-			num_octets = length & 0x7f
-			octets = data[tag.length + 1, num_octets].unpack('C*')
+    if length < 0x80
+      self[tag_i] = data[tag.length + 1, length]
+      remainder = data[tag.length + 1 + length..-1]
+    else
+      num_octets = length & 0x7f
+      octets = data[tag.length + 1, num_octets].unpack('C*')
 
-			length = 0
-  		octets.reverse.each_with_index {|octet, idx|
-    		length += octet * (256**idx)
-  		}
+      length = 0
+      octets.reverse.each_with_index {|octet, idx|
+        length += octet * (256**idx)
+      }
 
-			self[tag_i] = data[tag.length + 1 + num_octets, length]
-			remainder = data[tag.length + 1 + num_octets + length..-1]
-		end
+      self[tag_i] = data[tag.length + 1 + num_octets, length]
+      remainder = data[tag.length + 1 + num_octets + length..-1]
+    end
 
-		if remainder and remainder.length > 2
-			self.from_ber_tlv(remainder)
-		end
+    if remainder and remainder.length > 2
+      self.from_ber_tlv(remainder)
+    end
 
-		self
-	end
+    self
+  end
 
-	def to_ber_tlv
-		result = ''
-		self.each do |tag, value|
-		  v = value.dup
-			result << int_to_binstr(tag) # adding tag to result string
+  def to_ber_tlv
+    result = ''
+    self.each do |tag, value|
+      v = value.dup
+      result << int_to_binstr(tag) # adding tag to result string
 
-			#adding size of the tag
-			if v.size < 0x80
-				result << int_to_binstr(v.size)
-			else
-				len_len = int_to_binstr(v.size)
-				result << int_to_binstr(0x80 | len_len.size)
-				result << len_len
-			end
+      #adding size of the tag
+      if v.size < 0x80
+        result << int_to_binstr(v.size)
+      else
+        len_len = int_to_binstr(v.size)
+        result << int_to_binstr(0x80 | len_len.size)
+        result << len_len
+      end
 
-			result << v
-		end
+      result << v
+    end
 
-		result
-	end
+    result
+  end
 
-	# converts integer to binary string representation
-	def int_to_binstr(i)
-		binstr = "%X" % i
-		binstr = '0' << binstr if binstr.size % 2 == 1
-		[binstr].pack('H*').force_encoding("UTF-8")
-	end
+  # converts integer to binary string representation
+  def int_to_binstr(i)
+    binstr = "%X" % i
+    binstr = '0' << binstr if binstr.size % 2 == 1
+    [binstr].pack('H*')
+  end
 end
-
--- test/unit/hash_ber_tlv_test.rb
+++ test/unit/hash_ber_tlv_test.rb
@@ -9,5 +9,11 @@ class HashBerTLVTest < DaFunk::Test.case
     hash = Hash.new
     assert_equal hash_tlv, hash.from_ber_tlv("\x9F7\x04u\xCB\x11\xD1")
   end
+
+  def test_to_big_ber_tlv
+    tlv  = "5F2A02098682021800950580000080009A031707129C01009F02060000000015009F10120014A50003020000000000000000000000FF9F1A0200769F26082568341CC4B3CEBA9F2701809F34034103029F360200519F3303E0F0E89F3704D1F5D3CC9F1E0800000000000000009F1E083541313739363135"
+    hash = {24362=>"\t\x86", 130=>"\x18\x00", 149=>"\x80\x00\x00\x80\x00", 154=>"\x17\a\x12", 156=>"\x00", 40706=>"\x00\x00\x00\x00\x15\x00", 40720=>"\x00\x14\xA5\x00\x03\x02\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\xFF", 40730=>"\x00v", 40742=>"%h4\x1C\xC4\xB3\xCE\xBA", 40743=>"\x80", 40756=>"A\x03\x02", 40758=>"\x00Q", 40755=>"\xE0\xF0\xE8", 40759=>"\xD1\xF5\xD3\xCC", 40734=>"5A179615"}
+    assert_equal hash, Hash.new.from_ber_tlv([tlv].pack("H*"))
+  end
 end
 
